b40cbb8b54ff346b650e9da07f50821a43e5e2d3
hyemin33 <joliie@naver.com>
2023-02-27 14:46:43 +0900

    feat: apollo-client 사용 및 _app.js 원리 체크

---
+++ class/pages/graphql-mutation/index.js
@@ -0,0 +1,26 @@
+import { gql, useMutation } from "@apollo/client";
+
+export default function GraphqlMutationPage() {
+  const CREATE_BOARD = gql`
+    mutation {
+      createBoard(
+        writer: "혜민"
+        title: "오늘 점심메뉴 알림"
+        contents: "오늘 점심은 고등어 구이 입니다. 맛있게 드세요~"
+      ) {
+        _id
+        number
+        message
+      }
+    }
+  `;
+
+  const [my] = useMutation(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await my();
+    alert(result.data.createBoard.message);
+  };
+
+  return <button onClick={onClickSubmit}>GRAPHQL-API(동기)</button>;
+}
