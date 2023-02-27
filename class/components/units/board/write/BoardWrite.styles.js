dabcead24ef369538dd66fb39ae60a6100332405
hyemin33 <joliie@naver.com>
2023-02-27 18:09:30 +0900

    feat: 02-02-graphql-mutation-args 파일분리 해서 구현하기

---
+++ class/components/units/board/write/BoardWrite.styles.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Button = styled.button`
+  border: 1px solid #333;
+  border-radius: 4px;
+  background: none;
+  padding: 10px;
+  margin-top: 20px;
+`;
