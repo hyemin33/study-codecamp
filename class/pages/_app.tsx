a4967b8b73fa911386118b4e3ab04cf97abaa7e6
hyemin33 <joliie@naver.com>
2023-03-04 14:58:19 +0900

    fix: firebase 파일분리

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -3,26 +3,6 @@ import { AppProps } from "next/app";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 import ApolloSetting from "../src/components/commons/apollo";
 
-// ////////////////////////////
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDBsA8xCMPc4cEJnzt0JIhskIv4QZy85OQ",
-  authDomain: "hm-test-eb640.firebaseapp.com",
-  projectId: "hm-test-eb640",
-  storageBucket: "hm-test-eb640.appspot.com",
-  messagingSenderId: "795555233167",
-  appId: "1:795555233167:web:7f53c44441411a05afdffa",
-};
-
-// Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-// ////////////////////////////
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloSetting>
