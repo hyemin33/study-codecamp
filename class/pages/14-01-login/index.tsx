a4e99918447539044a93da6749d4671bc6a9f740
hyemin33 <joliie@naver.com>
2023-03-05 19:01:07 +0900

    feat: 로그인 페이지 추가

---
+++ class/pages/14-01-login/index.tsx
@@ -0,0 +1,66 @@
+import { gql, useMutation } from "@apollo/client";
+import { Modal } from "antd";
+import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../src/commons/store";
+import {
+  IMutation,
+  IMutationLoginUserArgs,
+} from "../../src/commons/types/generated/types";
+
+const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginUser] = useMutation(LOGIN_USER);
+
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onClickLogin = async () => {
+    try {
+      const result = await loginUser({
+        variables: {
+          email,
+          password,
+        },
+      });
+
+      const accessToken = result?.data?.loginUser.accessToken;
+      if (!accessToken) {
+        Modal.error({ content: "로그인에 실패했습니다. 다시 시도해주세요." });
+        return;
+      }
+      setAccessToken(accessToken);
+
+      void router.push(`/14-02-login-success`);
+    } catch (error) {
+      if (error instanceof Error) Modal.error({ content: error.message });
+    }
+  };
+
+  return (
+    <>
+      이메일 <input type="text" onChange={onChangeEmail} />
+      패스워드 <input type="password" onChange={onChangePassword} />
+      <button onClick={onClickLogin}>로그인하기</button>
+    </>
+  );
+}
--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -1,4 +1,6 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { accessTokenState } from "../../../commons/store";
+import { useRecoilState } from "recoil";
 
 // 밖에서 cache를 하기 때문에 페이지 전환 되어도(_app.tsx 리렌더) 캐시 유지
 const GLOBAL_STATE = new InMemoryCache();
@@ -8,9 +10,13 @@ interface IApolloSettingProps {
 }
 
 export default function ApolloSetting(props: IApolloSettingProps) {
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
   const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
+    // uri: "http://practice.codebootcamp.co.kr/graphql",
+    uri: "http://backendonline.codebootcamp.co.kr/graphql",
     cache: GLOBAL_STATE,
+    headers: { Authorization: `Bearer ${accessToken}` },
   });
 
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
