4ca08262367feda3bf89c6508bd6d0f9abda980f
hyemin33 <joliie@naver.com>
2023-03-05 23:19:14 +0900

    feat: 권한분기 HOF, HOC 사용하기, withAuth 사용하기

--- class/pages/14-02-login-success/index.tsx
+++ class/pages/14-02-login-success/index.tsx
@@ -1,19 +1,22 @@
 import { gql, useQuery } from "@apollo/client";
 import { IQuery } from "../../src/commons/types/generated/types";
+import { withAuth } from "../../src/components/commons/hocs/withAuth";
 
 const FETCH_USER_LOGED_IN = gql`
   query fetchUserLoggedIn {
     fetchUserLoggedIn {
-      _id
       email
       name
     }
   }
 `;
 
-export default function LoginSuccessPage() {
+function LoginSuccessPage() {
   const { data } =
     useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGED_IN);
 
   return <>{data?.fetchUserLoggedIn?.name}님 환영합니다!</>;
 }
+
+//hoc 사용
+export default withAuth(LoginSuccessPage);
