083abc3d3d9acae5fbf70b4ac625315788c5216f
hyemin33 <joliie@naver.com>
2023-03-06 23:44:06 +0900

    feat: observable

---
+++ class/pages/17-03-observable-flatmap/index.tsx
@@ -0,0 +1,17 @@
+import { Observable } from "@apollo/client";
+import { from } from "zen-observable";
+
+export default function ObservableFlatmapPage() {
+  const onClickButton = () => {
+    // new Promis(()=>{})
+    //new Observable(()=>{}) 검토단계..
+
+    from(["1번 useQuery", "2번 useQuery", "3번 useQuery"]) // fromPromise
+      .flatMap((el: string) =>
+        from([`${el} 결과에 qqq 적용`, `${el} 결과에 zzz 적용`])
+      )
+      .subscribe((el) => console.log(el));
+  };
+
+  return <button onClick={onClickButton}>클릭</button>;
+}
